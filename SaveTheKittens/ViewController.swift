d404d03f157757f739db0db4b2b4e257162af533
Cole Cooper <ccooper@ncompasstrac.com>
2019-06-21 14:19:39 -0700

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Cole Cooper on 6/21/19.
+//  Copyright © 2019 Cole Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
